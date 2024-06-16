a9241c020a6d81029ea3e0fe3b6dc834aa6561ac
EC2 Default User <ec2-user@ip-172-31-37-195.ap-northeast-1.compute.internal>
2024-06-16 12:47:56 +0000

    end

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,9 +1,9 @@
 class Public::CustomersController < ApplicationController
   def index
-   @profile_my=Profile.find(params[:id])
+   #@public=Public.find(params[:id])
    @posts_my=Post.all
   end
-  
+
   def create
    @profile_my=Profile.new(user_params)
    if @profile_my.save
@@ -13,14 +13,14 @@ class Public::CustomersController < ApplicationController
     render :index
    end
   end
-  
+
   def edit
    @profile_my=Profile.find(params[:id])
    if @profile_my.id !=current_user.id
     redirect_to user_posts_path(current_user.id)
    end
   end
-  
+
   def update
    @profile_my=Profile.find(params[:id])
    if @profile_my.update(user_params)
@@ -30,5 +30,12 @@ class Public::CustomersController < ApplicationController
     render:edit
    end
   end
-  
+
+  private
+
+  def post_new_params
+   params.require(:public).permit(:name, :introduction)
+  # require:送られてきたデータの中からモデル名を指定し、データんを絞り込む。　permit:requireで絞り込んだデータの中から、保存を許可するカラムを指定します。
+  end
+
 end
