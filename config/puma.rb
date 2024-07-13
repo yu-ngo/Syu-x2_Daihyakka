37028b349994ed49e8c15670679ec2b18be0f37e
EC2 Default User <ec2-user@ip-172-31-37-195.ap-northeast-1.compute.internal>
2024-07-13 11:02:36 +0000

    deploy setting

--- config/puma.rb
+++ config/puma.rb
@@ -43,16 +43,16 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 plugin :tmp_restart
 
 bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
-rails_root=Dir.pwd
-#本番環境のみデーモン起動
+rails_root = Dir.pwd
+# 本番環境のみデーモン起動
 if Rails.env.production?
   pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
   state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
   stdout_redirect(
-   File.join(rails_root, 'log', 'puma.log'),
-   File.join(rails_root, 'log', 'puma-error.log'),
-   true
+    File.join(rails_root, 'log', 'puma.log'),
+    File.join(rails_root, 'log', 'puma-error.log'),
+    true
   )
-    #デーモン
-    daemonize
+  # デーモン
+  daemonize
 end
